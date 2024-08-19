1b380a94cb99b113be013cc9bc5593e1d3a5e31d
Sindhu M <Sindhu.M@godigit.com>
2024-08-19 17:12:14 +0530

    Sign-In in fundoo notes in Google keep

---
+++ src/app/archieve-container/archieve-container.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../services/notes-service/notes-service.service';
+import { error } from 'winston';
+
+
+@Component({
+  selector: 'app-archieve-container',
+  templateUrl: './archieve-container.component.html',
+  styleUrls: ['./archieve-container.component.scss'],
+})
+export class ArchieveContainerComponent implements OnInit {
+  notesList: any = [];
+
+  constructor(private notesService: NotesService) {}
+
+  ngOnInit(): void {
+    this.notesService.getAllNotesApiCall('getArchiveNotesList').subscribe({
+      next: (r: any) => {
+        this.notesList = r.data.data;
+        console.log('result is: ', this.notesList);
+      },
+      error : (e) => {},
+      // error: (e) => {},
+    });
+  }
+}
